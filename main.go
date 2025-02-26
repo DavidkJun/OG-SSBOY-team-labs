7d5d95ba38b5ca768dd43a719d4be6437041c9ff
Lev <lev.bereza@gmail.com>
2025-02-26 21:35:50 +0200

    Updated by Lev

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 package main
 
 import (
@@ -11,3 +12,10 @@ type TimeResponse struct {
   Time string json:"time"
 }
 >>>>>>> 1a2c3a4 (Added feature Yarik)
+=======
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
+w.Header().Set("Content-Type", "application/json")
+json.NewEncoder(w).Encode(response)
+}
+>>>>>>> bd328e9 (Updated by Lev)
