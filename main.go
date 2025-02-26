d8e424fc68beb7ad8208ab6e442cdc0927ae2e82
Lev <lev.bereza@gmail.com>
2025-02-26 22:32:20 +0200

    Added module and fixed server

--- main.go
+++ main.go
@@ -1,28 +1,22 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 package main
 
 import (
-  "encoding/json"
-  "net/http"
-  "time"
+	"encoding/json"
+	"net/http"
+	"time"
 )
-=======
+
 type TimeResponse struct {
-  Time string json:"time"
+	Time string `json:"time"`
 }
->>>>>>> 1a2c3a4 (Added feature Yarik)
-=======
+
 func timeHandler(w http.ResponseWriter, r *http.Request) {
-response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
-w.Header().Set("Content-Type", "application/json")
-json.NewEncoder(w).Encode(response)
+	response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
->>>>>>> bd328e9 (Updated by Lev)
-=======
+
 func main() {
 	http.HandleFunc("/time", timeHandler)
 	http.ListenAndServe(":8795", nil)
 }
->>>>>>> d46f04c (added artem feature)
